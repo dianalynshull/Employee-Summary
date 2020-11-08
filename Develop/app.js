0de9c091f2b0d0e7897dded7adfea200d90cc371
dianalynshull <dianalynshull@gmail.com>
2020-11-08 00:22:34 -0700

    completed the engineer and intern inquirer questions and successfully pushed the answers to the employees array

--- Develop/app.js
+++ Develop/app.js
@@ -14,7 +14,19 @@ const render = require("./lib/htmlRenderer");
 const managerQuestion = {
   type: "input",
   name: "officeNumber",
-  message: "Please enter the office number of the manager",
+  message: "Please enter the office number of the manager: ",
+};
+
+const engineerQuestion = {
+  type: "input",
+  name: "github",
+  message: "Please enter the github username of the engineer: ",
+};
+
+const internQuestion = {
+  type: "input",
+  name: "school",
+  message: "Please enter the school that the intern attended: ",
 };
 
 const genericQuestions = [
@@ -35,12 +47,37 @@ const genericQuestions = [
   }
 ];
 
+const newEmployeeOptions = {
+  type: "list",
+  name: "role",
+  choices: [
+    "Intern",
+    "Engineer",
+    "Done",
+  ],
+}
+
 function managerQuestionFunction() {
   return inquirer.prompt(managerQuestion);
 };
 
+function engineerQuestionFunction() {
+  console.log("Please answer the following questions about the engineer that you are adding to the team: ")
+  return inquirer.prompt(engineerQuestion);
+};
+
+function internQuestionFunction() {
+  console.log("Please answer the following questions about the intern that you are adding to the team: ")
+  return inquirer.prompt(internQuestion);
+};
+
 function genericQuestionsFunction() {
   return inquirer.prompt(genericQuestions);
+};
+
+function addEmployee() {
+  console.log("Would you like to add an employee to the manager's team? If not, please select 'Done'");
+  return inquirer.prompt(newEmployeeOptions)
 };
 
 async function createManager() {
@@ -52,14 +89,59 @@ async function createManager() {
     employees.push(manager);
     console.log(employees);
 
+    createEmployee();
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+async function createEmployee() {
+  try {
+    const employeeRole = await addEmployee();
+    if (employeeRole.role === "Intern") {
+      createIntern();
+    } else if (employeeRole.role === "Engineer") {
+      createEngineer();
+    } else if (employeeRole.role === "Done") {
+      console.log("All done!");
+      return;
+    }
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+async function createEngineer() {
+  try {
+    const engineerAnswers = await engineerQuestionFunction();
+    const genericEngineerAnswers = await genericQuestionsFunction();
+    const engineer = new Engineer(genericEngineerAnswers.name, genericEngineerAnswers.id, genericEngineerAnswers.email, engineerAnswers.github);
+
+    employees.push(engineer);
+    console.log(employees);
+
+    createEmployee();
+  } catch(err) {
+    console.log(err);
+  }
+}
+async function createIntern() {
+  try {
+    const internAnswers = await internQuestionFunction();
+    const genericInternAnswers = await genericQuestionsFunction();
+    const intern = new Intern(genericInternAnswers.name, genericInternAnswers.id, genericInternAnswers.email, internAnswers.school);
+
+    employees.push(intern);
+    console.log(employees);
+    createEmployee();
   } catch(err) {
     console.log(err);
   }
 }
 
 async function init() {
-  createManager();
-  
+  console.log("Welcome to the Team Profile Generator! We will start by adding the manager's information: ")
+  createManager(); 
 }
 
 init();
